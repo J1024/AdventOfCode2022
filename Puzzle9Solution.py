5e85257596ade3feca4a656e404bd2a5e293a34f
J1024 <31326139+J1024@users.noreply.github.com>
2022-12-09 23:16:23 -0600

    Final solution for Puzzle 9.

--- Puzzle9Solution.py
+++ Puzzle9Solution.py
@@ -1,168 +1,78 @@
 #################################
-###Advent of Code Day 8
-###Start Time: 10:00am
-###End Time: 11:30am
+###Advent of Code Day 9
+###Start Time: 3:00pm
+###End Time: 11:15pm
 ###Author: Jonathan LeFeber
-###Lines of Code: 109
+###Lines of Code: 71
 #################################
 import time
-headLocation = [0,0]
-tailLocation = [0,0]
+numKnots = 10
+ropeLocation = []
+for x in range(numKnots):
+    ropeLocation.append([0,0])
 hasBeen = ["0/0"]
 f = open("Puzzle9Input.txt", "r")
 for line in f:
     line = line.strip()
     moves = int(line[2:])
     direction = line[:1]
-    #print("Starting Move Set:")
-    #print("Head is at: ",headLocation)
-    #print("Tail is at: ",tailLocation)
-    #print("Moving ",moves," times in ",direction," direction")
     for move in range(moves):
         #MoveHead
         if direction == "L":
-            headLocation[0] -= 1
+            ropeLocation[0][0] -= 1
         elif direction == "R":
-            headLocation[0] += 1
+            ropeLocation[0][0] += 1
         elif direction == "U":
-            headLocation[1] += 1
+            ropeLocation[0][1] += 1
         elif direction == "D":
-            headLocation[1] -= 1
-        #MoveTail
-        #If I'm 2 away or greater, I need to catchup
-        if abs(headLocation[0] - tailLocation[0]) > 1:
-            if direction == "L":
-                tailLocation[0]=headLocation[0]+1
-                tailLocation[1]=headLocation[1]
-            elif direction == "R":
-                tailLocation[0]=headLocation[0]-1
-                tailLocation[1]=headLocation[1]
-        elif abs(headLocation[1] - tailLocation[1]) > 1:
-            if direction == "D":
-                tailLocation[1]=headLocation[1]+1
-                tailLocation[0]=headLocation[0]
-            elif direction == "U":
-                tailLocation[1]=headLocation[1]-1
-                tailLocation[0]=headLocation[0]
-        tailString = str(tailLocation[0])+"/"+str(tailLocation[1])
-        if tailString not in hasBeen:
-            hasBeen.append(tailString)
-        
-        #print("After ",move+1," move(s), Head is at: ",headLocation)
-        #print("After ",move+1," move(s), Tail is at: ",tailLocation)
-
-    #print("")
-#print(hasBeen)
-print("Tail has been in ",len(hasBeen)," locations.")
-
-
-"""
-
-forrest = []
-forrestLine = []
-visibleTrees = 0
-f = open("Puzzle8Input.txt", "r")
-x = 0
-y = 0
-for line in f:
-    forrestLine = []
-    for c in line.strip():
-        #print(c)
-        forrestLine.append([int(c),' '])
-    forrest.append(forrestLine)
-    y += 1
-    
-
-#for z in range(len(forrest)):
-#    print(forrest[z])
-
-
-#Check Left-to-Right
-for y in range(1,len(forrest)-1):
-    tallest = forrest[y][0][0]
-    for x in range(1, len(forrestLine)-1):
-        if forrest[y][x][0] > forrest[y][x-1][0] and forrest[y][x][0] > tallest:
-            #print("1st > 2nd: ",forrest[y][x][0],forrest[y][x-1][0])
-            tallest =  forrest[y][x][0]
-            forrest[y][x][1] = "x"
-
-#Check Right-to-Left
-for y in range(1,len(forrest)-1):
-    tallest = forrest[y][len(forrestLine)-1][0]
-    for x in range(len(forrestLine)-2,0,-1):
-        if forrest[y][x][0] > forrest[y][x+1][0] and forrest[y][x][0] > tallest:
-            #print("1st > 2nd: ",forrest[y][x][0],forrest[y][x+1][0])
-            tallest =  forrest[y][x][0]
-            forrest[y][x][1] = "x"
-
-#Check Up-to-Down
-for x in range(1,len(forrestLine)-1):
-    tallest = forrest[0][x][0]
-    for y in range(1,len(forrest)-1):
-        if forrest[y][x][0] > forrest[y-1][x][0] and forrest[y][x][0] > tallest:
-            #print("1st > 2nd: ",forrest[y][x][0],forrest[y-1][x][0])
-            tallest =  forrest[y][x][0]
-            forrest[y][x][1] = "x"
-
-#Check Down-to-Up
-for x in range(1,len(forrestLine)-1):
-    tallest = forrest[len(forrest)-1][x][0]
-    for y in range(len(forrest)-2,0,-1):
-        if forrest[y][x][0] > forrest[y+1][x][0] and forrest[y][x][0] > tallest:
-            #print("1st > 2nd: ",forrest[y][x][0],forrest[y+1][x][0])
-            tallest =  forrest[y][x][0]
-            forrest[y][x][1] = "x"
-
-
-#Visible Count
-edgeTrees = (2*len(forrest))+(2*len(forrestLine))-4
-print("Edge Trees: ",edgeTrees)
-#Count the Xs
-for y in range(len(forrest)):
-    for x in range(len(forrestLine)):
-        if forrest[y][x][1] == "x":
-            edgeTrees += 1
-print("Total Visible Trees: ",edgeTrees)
-
-#Best TreeHouse Location Go:
-for y in range(len(forrest)):
-    for x in range(len(forrestLine)):
-        if y != 0 and x != 0 and y != len(forrest)-1 and x != len(forrestLine)-1:
-            #Checking values for forrest[y][x]:
-            treeHeight = forrest[y][x][0]
-            North = 0
-            South = 0
-            East = 0
-            West = 0
-            #Check North
-            for why in range(y,0,-1):
-                North += 1
-                if forrest[why-1][x][0] >= forrest[y][x][0]:
-                    break
-            #Check West
-            for ex in range(x,0,-1):
-                West += 1
-                if forrest[y][ex-1][0] >= forrest[y][x][0]:
-                    break
-            #Check East
-            for ex in range(x,len(forrestLine)-1):
-                East += 1
-                if forrest[y][ex+1][0] >= forrest[y][x][0]:
-                    break
-            #Check South
-            for why in range(y,len(forrest)-1):
-                South += 1
-                if forrest[why+1][x][0] >= forrest[y][x][0]:
-                    break
-            socialScore = North*South*East*West
-            forrest[y][x][1] = socialScore
-
-#Find Largest Social Score TODO: I could build this into the above easily enough.... saves having to walk the array AGAIN.
-best = 0
-for y in range(len(forrest)):
-    for x in range(len(forrestLine)):
-        if forrest[y][x][1] != ' ' and int(forrest[y][x][1]) > best:
-            best = int(forrest[y][x][1])
-print("Best Social Score: ",best)
-"""
+            ropeLocation[0][1] -= 1
+        #Loop through all following knots
+        for knotNum in range(1,numKnots):
+            #MoveTail(s)
+            #If I'm 2 away or greater, I need to move
+            if abs(ropeLocation[knotNum-1][0] - ropeLocation[knotNum][0]) > 1 or abs(ropeLocation[knotNum-1][1] - ropeLocation[knotNum][1]) > 1:
+                #If I'm >= 1 away in two directions, I need to move diagonally
+                if abs(ropeLocation[knotNum-1][0] - ropeLocation[knotNum][0]) >= 1 and abs(ropeLocation[knotNum-1][1] - ropeLocation[knotNum][1]) >= 1:
+                    if ropeLocation[knotNum-1][0] > ropeLocation[knotNum][0] and ropeLocation[knotNum-1][1] > ropeLocation[knotNum][1]:
+                        #x and y + 1
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]+1
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]+1
+                    elif ropeLocation[knotNum-1][0] < ropeLocation[knotNum][0] and ropeLocation[knotNum-1][1] < ropeLocation[knotNum][1]:
+                        #x and y - 1
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]-1
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]-1
+                    elif ropeLocation[knotNum-1][0] > ropeLocation[knotNum][0] and ropeLocation[knotNum-1][1] < ropeLocation[knotNum][1]:
+                        #x + 1 and y - 1
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]+1
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]-1
+                    elif ropeLocation[knotNum-1][0] < ropeLocation[knotNum][0] and ropeLocation[knotNum-1][1] > ropeLocation[knotNum][1]:
+                        #x - 1 and y + 1
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]-1
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]+1
+                    else:
+                        print("How did I get here: 1")
+                #Otherwise I'm still 2 away, so just move towards numKnot-1    
+                else:
+                    if ropeLocation[knotNum-1][0] > ropeLocation[knotNum][0]:
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]+1
+                    elif ropeLocation[knotNum-1][0] < ropeLocation[knotNum][0]:
+                        ropeLocation[knotNum][0] = ropeLocation[knotNum][0]-1
+                    elif ropeLocation[knotNum-1][1] > ropeLocation[knotNum][1]:
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]+1
+                    elif ropeLocation[knotNum-1][1] < ropeLocation[knotNum][1]:
+                        ropeLocation[knotNum][1] = ropeLocation[knotNum][1]-1
+                    else:
+                        print("How did I get here: 2")
+                        print("Preceding Rope Location: ",ropeLocation[knotNum-1][0],ropeLocation[knotNum-1][1])
+                        print("My Rope Location:        ",ropeLocation[knotNum][0],ropeLocation[knotNum][1])
+            #Otherwise I'm not 2 away, didn't move, and can break 
+            else:
+                break
+            
+            #If I'm the tail, update tail tracker
+            if knotNum == numKnots-1:
+                tailString = str(ropeLocation[numKnots-1][0])+"/"+str(ropeLocation[numKnots-1][1])
+                if tailString not in hasBeen:
+                    hasBeen.append(tailString)
+print("For ",numKnots,"knots, the tail has been in ",len(hasBeen)," locations.")
 print(time.process_time())
